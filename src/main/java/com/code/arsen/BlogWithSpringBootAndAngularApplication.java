e2f80f8857c32dcf8790f6b8d856a2082f61094d
ibnu arseno <Ibnu.Arseno@bni.co.id>
2022-05-30 13:39:20 +0700

    add project

---
+++ src/main/java/com/code/arsen/BlogWithSpringBootAndAngularApplication.java
@@ -0,0 +1,13 @@
+package com.code.arsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWithSpringBootAndAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWithSpringBootAndAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/arsen/BlogWithSpringBootAndAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.arsen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWithSpringBootAndAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
